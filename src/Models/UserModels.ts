f0bb7d7a0eea1795bca91890069b8cc567aeb411
nicsylvia <nicsylvia15f@gmail.com>
2023-04-27 01:04:40 -0700

    saved user models

---
+++ src/Models/UserModels.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+import isEmail from "validator/lib/isEmail";
+import { iUser } from "../AllInterfaces/AllInterfaces";
+
+const UserSchema = new mongoose.Schema<iUser>(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please enter your email"],
+      lowercase: true,
+      trim: true,
+      validate: [isEmail, "Please enter a valid email"],
+    },
+    userName: {
+      type: String,
+      required: [true, "Please enter your Username"],
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your phone number"],
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+    },
+
+    predict: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "predicts",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<iUser>("Users", UserSchema);
