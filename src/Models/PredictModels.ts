e8c820c6aec4f6c6dde4bba649dfba376add5ae3
nicsylvia <nicsylvia15f@gmail.com>
2023-04-27 01:11:03 -0700

    saved predict match models

---
+++ src/Models/PredictModels.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+import { Ipredict } from "../AllInterfaces/AllInterfaces";
+
+interface predict extends Ipredict, mongoose.Document {}
+
+const predictSchema = new mongoose.Schema(
+  {
+    teamA: {
+      type: String,
+    },
+    teamB: {
+      type: String,
+    },
+
+    teamAScore: {
+      type: Number,
+    },
+
+    amount: {
+      type: Number,
+    },
+
+    prize: {
+      type: Number,
+    },
+
+    teamBScore: {
+      type: Number,
+    },
+
+    dateTime: {
+      type: String,
+    },
+
+    scoreEntry: {
+      type: String,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  { timestamps: true }
+);
+
+const PredictModel = mongoose.model<predict>("predicts", predictSchema);
+
+export default PredictModel;
