8706aa137447765dd9efac7afce3d3f9ee9ddc93
nicsylvia <nicsylvia15f@gmail.com>
2023-04-27 01:11:53 -0700

    saved match models

---
+++ src/Models/MatchModels.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema, model } from "mongoose";
+
+import { iMatch } from "../AllInterfaces/AllInterfaces";
+
+const MatchSchema: Schema<iMatch> = new Schema(
+  {
+    startPlay: {
+      type: Boolean,
+    },
+    stopPlay: {
+      type: Boolean,
+    },
+    teamA: {
+      type: String,
+    },
+    teamB: {
+      type: String,
+    },
+
+    teamAScore: {
+      type: Number,
+    },
+
+    teamBScore: {
+      type: Number,
+    },
+
+    Odds: {
+      type: Number,
+    },
+
+    dateTime: {
+      type: String,
+    },
+
+    scoreEntry: {
+      type: String,
+    },
+
+    predict: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "predicts",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const MatchModels = model<iMatch>("Matches", MatchSchema);
+
+export default MatchModels;
