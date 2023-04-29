ce06a1c0ca4f1c2d6be988d86d5c117ff7240457
nicsylvia <nicsylvia15f@gmail.com>
2023-04-29 01:26:59 -0700

    update and login user

--- src/Controllers/UserControllers.ts
+++ src/Controllers/UserControllers.ts
@@ -79,3 +79,66 @@ export const UsersRegistration = AsyncHandler(
     });
   }
 );
+
+// Users Login:
+export const UsersLogin = AsyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    const CheckEmail = await UserModels.findOne({ email });
+
+    if (!CheckEmail) {
+      next(
+        new AppError({
+          message: "User not Found",
+          httpcode: HTTPCODES.NOT_FOUND,
+        })
+      );
+    }
+
+    const CheckPassword = await bcrypt.compare(password, CheckEmail!.password);
+
+    if (!CheckPassword) {
+      next(
+        new AppError({
+          message: "Email or password not correct",
+          httpcode: HTTPCODES.CONFLICT,
+        })
+      );
+    }
+
+    if (CheckEmail && CheckPassword) {
+      return res.status(200).json({
+        message: "Login Successfull",
+        data: CheckEmail,
+      });
+    }
+  }
+);
+
+// Update one user:
+export const updateOneUser = AsyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userName } = req.body;
+
+    const user = await UserModels.findByIdAndUpdate(
+      req.params.userID,
+      { userName },
+      { new: true }
+    );
+
+    if (!user) {
+      next(
+        new AppError({
+          message: "An error occured in updating username",
+          httpcode: HTTPCODES.INTERNAL_SERVER_ERROR,
+        })
+      );
+    }
+
+    return res.status(201).json({
+      message: "Successfully updated the user's username",
+      data: user,
+    });
+  }
+);
