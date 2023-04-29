c99ae7090b09d8ad920ce3e767ebbe423a97daa4
nicsylvia <nicsylvia15f@gmail.com>
2023-04-29 01:26:25 -0700

    user registration saved

--- src/Controllers/UserControllers.ts
+++ src/Controllers/UserControllers.ts
@@ -43,3 +43,39 @@ export const GetSingleUser = AsyncHandler(
     });
   }
 );
+
+// Users Registration:
+export const UsersRegistration = AsyncHandler(
+  async (req: any, res: Response, next: NextFunction) => {
+    const { name, email, phoneNumber, userName, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const findEmail = await UserModels.findOne({ email });
+
+    if (findEmail) {
+      next(
+        new AppError({
+          message: "User with this account already exists",
+          httpcode: HTTPCODES.FORBIDDEN,
+        })
+      );
+    }
+
+    const Users = await UserModels.create({
+      name,
+      email,
+      userName,
+      phoneNumber: "234" + phoneNumber,
+      password: hashedPassword,
+      confirmPassword: hashedPassword,
+      status: "User",
+    });
+
+    return res.status(201).json({
+      message: "Successfully created User",
+      data: Users,
+    });
+  }
+);
