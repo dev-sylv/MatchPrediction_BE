a3e8a053c27b2b5a867deb3c3252dbbc66ccdf9c
nicsylvia <nicsylvia15f@gmail.com>
2023-04-29 20:44:29 -0700

    saved user schemq validation file

---
+++ src/Middlewares/Validation/UserSchemaValidation.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const UserSchemaValidation = {
+  Register: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    userName: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+    confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+  }),
+  Login: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
