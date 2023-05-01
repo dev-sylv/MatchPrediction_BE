819feefd2109fb9e61e1df454943b2d9b151b1ac
nicsylvia <nicsylvia15f@gmail.com>
2023-04-30 18:45:56 -0700

    async handler saved

---
+++ src/Utils/AsyncHandler.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+const AsyncHandler =
+  (fn: any) => (req: Request, res: Response, next: NextFunction) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
+
+export default AsyncHandler;
