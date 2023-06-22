01d8551a9436a14d7e18f85ff6712eb89d82f310
nicsylvia <nicsylvia15f@gmail.com>
2023-06-22 14:35:19 -0700

    saved db connect

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 import { EnvironmentalVariables } from "./EnvironmentVariables";
 
-// const LIVEURI = EnvironmentalVariables.MONGODB_STRING;
-const LIVEURI = "mongodb://0.0.0.0:27017/FootballPredictionServer";
+const LIVEURI = EnvironmentalVariables.MONGODB_STRING;
+// const LIVEURI = "mongodb://0.0.0.0:27017/FootballPredictionServer";
 
 export const DBCONNECTION = async () => {
   try {
